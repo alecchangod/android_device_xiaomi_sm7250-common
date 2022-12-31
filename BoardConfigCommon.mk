ce232eca5078867558575ea8b902791d179852ae
alecchangod <alec3d02@gmail.com>
2023-01-01 04:02:51 +0800

    inital commit for xtended

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -89,7 +89,7 @@ BOARD_HAVE_QCOM_FM := true
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(COMMON_PATH)/framework_compatibility_matrix.xml \
-    vendor/aosp/config/device_framework_matrix.xml
+    vendor/xtended/config/device_framework_matrix.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
@@ -106,6 +106,7 @@ TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-l
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sm7250
+TARGET_KERNEL_OPTIONAL_LD := true
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 134217728
