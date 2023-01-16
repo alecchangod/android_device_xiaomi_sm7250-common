00e87dcba52d813386d7cfe644601e7aab4d0d53
Alec Chan <alec3d02@gmail.com>
2023-01-16 20:34:31 +0000

    sm8250-common: Vibrator: sync package name with Android.bp file

--- common.mk
+++ common.mk
@@ -470,7 +470,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi_kona
+    vendor.qti.hardware.vibrator.service.xiaomi_lito
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
