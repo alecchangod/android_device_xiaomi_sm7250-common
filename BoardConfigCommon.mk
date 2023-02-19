9d480b97361bbcb395164345a1c4970ebfa0088a
Alec Chan <88945170+alecchangod@users.noreply.github.com>
2023-02-19 17:18:44 +0800

    sm7250-common: update path for trb clang

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -99,7 +99,7 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm7250
 KERNEL_SUPPORTS_LLVM_TOOLS := true
 TARGET_KERNEL_LLVM_BINUTILS := true
 TARGET_KERNEL_CLANG_VERSION := trb
-TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-latest
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-trb
 else
 TARGET_KERNEL_CLANG_VERSION := vangogh
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-vangogh
