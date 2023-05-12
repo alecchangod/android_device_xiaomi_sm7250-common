35a6fdac92f161413f7e412410116e878530352e
Alec Chan <alec3d02@gmail.com>
2023-05-12 23:09:42 +0800

    sm7250-common: import device_framework_matrix.xml

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -77,7 +77,7 @@ BOARD_HAVE_QCOM_FM := true
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(COMMON_PATH)/framework_compatibility_matrix.xml \
-        vendor/xtended/config/device_framework_matrix.xml
+        $(COMMON_PATH)/device_framework_matrix.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
