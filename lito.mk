79192878327392c430ebd1823b62089825ef63ab
Alec Chan <alec3d02@gmail.com>
2023-07-04 02:04:38 +0800

    Revert "sm7250-common: add dialer_experience.xml to fix Google Phone" Doesn't work :( This reverts commit c458ae190515e1067b55dc64648e3fc2c72f9dbb.

--- lito.mk
+++ lito.mk
@@ -21,10 +21,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
-# Fix Dialer
-PRODUCT_COPY_FILES +=  \
-    $(LOCAL_PATH)/configs/etc/sysconfig/dialer_experience.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/dialer_experience.xml
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.audio.low_latency.xml \
