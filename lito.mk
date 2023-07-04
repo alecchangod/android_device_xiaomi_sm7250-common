593b6be405b616d2dd6ce770b185922aa4f4166e
Alec Chan <alec3d02@gmail.com>
2023-07-04 09:44:56 +0800

    sm7250-common: build misys packages

--- lito.mk
+++ lito.mk
@@ -166,6 +166,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service_64 \
+    android.hidl.memory@1.0.vendor \
+    android.hidl.memory.block@1.0.vendor \
     libcamera2ndk_vendor \
     libgui_vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
@@ -262,6 +264,7 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
+    android.hidl.manager@1.0.vendor \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
