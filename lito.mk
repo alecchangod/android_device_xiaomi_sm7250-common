5f73bcaaef5bbef075e4176e0e08fb1bdf5f675f
Alec Chan <alec3d02@gmail.com>
2024-03-26 21:06:31 +0800

    sm7250-common: switch to android.hardware.usb@1.3-service.dual_role_usb

--- lito.mk
+++ lito.mk
@@ -239,8 +239,7 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0.vendor \
     libhidltransport.vendor \
-    libhwbinder.vendor \
-    vndservicemanager
+    libhwbinder.vendor
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
@@ -454,7 +453,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.3-service-qti
+    android.hardware.usb@1.3-service.dual_role_usb
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
@@ -462,6 +461,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/qcom/opensource/usb/etc
+
+# Vendor service manager
+PRODUCT_PACKAGES += \
+    vndservicemanager
 
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
